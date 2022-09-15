3a58ffccd289bac30e82d045b0187c04fcdffe4d
Rajat Khatri <khatrirajat888@gmail.com>
2022-09-15 16:31:34 +0530

    first commit with QPlay homepage
    version 0.0.1

---
+++ src/component/homePage/HomePage.js
@@ -0,0 +1,10 @@
+import './homePage.css';
+import Header from "./Header";
+import Carousel from "./Carousel";
+import Rail from "./Rail";
+
+export default () => <div id="homePage">
+    <Header />
+    <Carousel />
+    <Rail />
+</div>
